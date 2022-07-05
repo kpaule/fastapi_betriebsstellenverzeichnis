f3ad9b3d907f620e023b5d21a1865c080b3edd83
kpaule <kai@paule-online.de>
2022-07-05 16:50:01 +0200

    add function for loading csv to sql

---
+++ betriebsstellenverzeichnis/csv_loader.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+from .database import engine
+
+
+def load_csv(file_name):
+    with open(file_name, "r") as file:
+        col_names = ["plc", "rl100_code", "rl100_name", "rl100_name_short", "type_short", "type",
+                     "operating_status", "start_date", "end_date", "branch", "regional_area", "last_modified"]
+        data = pd.read_csv(file, sep=";", header=None,
+                           skiprows=1, names=col_names)
+    data.to_sql("betriebsstelle", con=engine, index=True,
+                index_label="id", if_exists="replace")
